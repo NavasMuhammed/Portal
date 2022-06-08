b41cbf53f6b0a90ad48243313d145f57e21b3ec8
Muhammed Navas <navas3444a3@gmail.com>
2022-06-08 21:31:26 +0530

    firs commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import '../styles/test.scss'
+import '../styles/topbar.scss'
+import '../styles/leftMenu.scss'
+import LeftMenu from './components/leftMenu/LeftMenu'
+import Topbar from './components/Topbar/Topbar.js'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+import Test from './Test'
+
+function MyApp() {
+  return (
+    <div className="tab">
+      {/* <Test /> */}
+      <Topbar />
+      <div className="mainBody">
+        <LeftMenu />
+      </div>
+    </div>
+  )
 }
 
 export default MyApp
